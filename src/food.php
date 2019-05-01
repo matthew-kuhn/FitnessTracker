c1762b6ee1570a1323afc597f20812ecdea3486f
Matthew <c20matthew.kuhn@usafa.edu>
2019-04-30 22:21:35 -0600

    add partial grocery functionality

--- src/food.php
+++ src/food.php
@@ -8,6 +8,43 @@
 			  	header("location:index.php");
 			  }
 
+
+			if(isset($_SESSION['myusername'])){
+			@ $db = new mysqli('localhost', 'root', '', 'FitnessTracker');
+
+			if (mysqli_connect_errno()) 
+             { 
+               echo 'ERROR: Could not connect to database.  Error is '.mysqli_connect_error();
+               exit;
+             }
+
+			$query = "SELECT NPlan FROM User WHERE UserID = ?";
+
+			$statement = $db->prepare($query);
+			$statement->bind_param('s', $_SESSION['myusername']);
+			$statement->execute(); 
+			$statement->store_result();
+			$statement->bind_result($_SESSION['NPlan']);
+			$statement->fetch();
+
+			if(strlen($_SESSION['NPlan']) > 1){
+
+			$query = "SELECT * FROM IN_NUTPLAN WHERE NUT_PLAN_NAME = ? ORDER BY Day ASC, Meal_num ASC;";
+
+			$statement = $db->prepare($query);
+			$statement->bind_param('s', $_SESSION['NPlan']);
+			$statement->execute();
+			$data = array();
+			$results = $statement->get_result();
+
+			while ($row = $results->fetch_assoc()) {
+				array_push($data, $row);
+			}
+		
+
+			$foodLoad = $data;
+
+		}
 ?>
 <html lang="en-US">
 	<head>
@@ -19,22 +56,89 @@
 		<link rel="stylesheet" type="text/css" href="fit.css">
 		<link rel="shortcut icon" type="image/png" href="favicon.ico">
 		<script type="text/javascript">
-			function pageUpdate(){
+			function pageUpdate(method){
+				var url = "food_update.php";
+				var request = new XMLHttpRequest();
+				request.onload = function() {
+		          var response = document.getElementById('weekly_meals');
+		          response.value = request.response;
+		          if(request.status == 404){
+		          	document.getElementById('weekly_meals').innerHTML = "<h2>Error 404</h2>";
+		          }else{
+		          	document.getElementById('weekly_meals').innerHTML = "<h2>Your Weekly Plan</h2>"; 
+		          }
+		          	document.getElementById('weekly_meals').innerHTML += "<table id='food_table' style='margin:auto'>";
+		          	var food = JSON.parse(request.response);
+		          	var groceries = food[1];
+		          	var food = food[0];
+		          	document.getElementById('grocery_list').innerHTML = groceries["Tilapia"];
+			        var prevDay = food[0]['Day'];
+			        var currDay = food[0]['Day'];
+			        var prevMeal = food[0]['Meal_num'];
+			        var currMeal = food[0]['Meal_num'];
+		          	for (var i = 0; i < food.length; i++) {
+			          	currDay = food[i]['Day'];
+			          	currMeal = food[i]['Meal_num'];
+			          	if(currDay != prevDay || i == 0){
+							switch(food[i]['Day']){
+								case 1:
+									document.getElementById('food_table').innerHTML += "<tr><th>Sunday</th><th></th><th></th></tr>";
+									break;
+								case 2:
+									document.getElementById('food_table').innerHTML +=  "<tr><th>Monday</th><th></th><th></th></tr>";
+									break;
+								case 3:
+									document.getElementById('food_table').innerHTML +=  "<tr><th>Tuesday</th><th></th><th></th></tr>";
+									break;
+								case 4:
+									document.getElementById('food_table').innerHTML +=  "<tr><th>Wednesday</th><th></th><th></th></tr>";
+									break;
+								case 5:
+									document.getElementById('food_table').innerHTML +=  "<tr><th>Thursday</th><th></th><th></th></tr>";
+									break;
+								case 6:
+									document.getElementById('food_table').innerHTML +=  "<tr><th>Friday</th><th></th><th></th></tr>";
+									break;
+								case 7:
+									document.getElementById('food_table').innerHTML +=  "<tr><th>Saturday</th><th></th><th></th></tr>";
+									break;
+							}
+						}
+						if(currMeal != prevMeal || i == 0){
+							switch(food[i]['Meal_num']){
+								case 1:
+									document.getElementById('food_table').innerHTML +=  "<tr><td></td><td>Breakfast:</td><td>"+food[i]['M_Name']+"</td></tr>";
+									break;
+								case 2:
+									document.getElementById('food_table').innerHTML +=  "<tr><td></td><td>Lunch:</td><td>"+food[i]['M_Name']+"</td></tr>";
+									break;
+								case 3:
+									document.getElementById('food_table').innerHTML +=  "<tr><td></td><td>Dinner:</td><td>"+food[i]['M_Name']+"</td></tr>";
+									break;
+							}
+						}else{
+							document.getElementById('food_table').innerHTML +=  "<tr><td></td><td></td><td>"+food[i]['M_Name']+"</td></tr>";
+						}
 
-			}
+						prevDay = currDay;
+						prevMeal = currMeal;
+					}
 
-
-			function planAction(action){
-				if(action == "food.php"){
-					alert(action);
-					//we need to change the php session variables to update the menus and grocery list
-					return true;
+					// document.getElementById('weekly_meals').innerHTML +=  "</table>";				
+				
+		          // document.getElementById('weekly_meals').innerHTML += "<p>"+request.response+"</p>";
+		          
+		          // var status = document.getElementById('status');
+		          // status.value = request.status;
+		        };
+				if(method == 'POST'){
+					var content = document.getElementById('mealPlan').value;
+					content = {planName : content};
+					content = JSON.stringify(content);
 				}
-				else{
-					//delete div, set up the form to set up a new plan, then 
-					alert(action);
-					return false;
-				}
+
+				request.open(method, url, true);
+        		request.send(content);
 			}
 		</script>
 	</head>
@@ -47,6 +151,7 @@
 				<a href="tracking.php">Tracking</a>
 				<a href="index.php">Home</a>
 				<a style="float: right" href="logout.php">Sign Out</a>
+				<p style="float: right"><?php echo $_SESSION['myusername'];?></p>
 			</nav>
 		</header>
 		<div id="grocery_list" style="text-align: center; width: 66%">
@@ -59,32 +164,89 @@
 				</ul>
 		</div>
 		 <div id="weekly_meals" style="text-align:center; width:66%">
-			<h2 >Your Weekly Menu</h2>
-			<ul style="display: inline-block; list-style: none ">
-					<li>Day 1: <?php echo "baked chicken breast every meal"?></li>
-					<li>Day 2: <?php echo "baked turkey breast every meal"?></li>
-					<li>Day 3: <?php echo "baked chicken wing every meal"?></li>
-					<li>Day 4: <?php echo "baked turkey wing every meal"?></li>
-					<li>Day 5: <?php echo "baked chicken breast every meal"?></li>
-					<li>Day 6: <?php echo "baked chicken wing every meal"?></li>
-					<li>Day 7: <?php echo "baked turkey breast every meal"?></li>
-				</ul>
+			<?php
+				if(strlen($_SESSION['NPlan']) < 1){
+			?>
+			<h2>Please Select a Plan</h2>
+
+			<?php		
+				}
+				else{
+					print "<h2>Your Weekly Plan</h2>";
+					print "<table style='margin: auto'>";
+					$prevDay = $foodLoad[0]['Day'];
+					$currDay = $foodLoad[0]['Day'];
+					$prevMeal = $foodLoad[0]['Meal_num'];
+					$currMeal = $foodLoad[0]['Meal_num'];
+					for($i = 0; $i < count($foodLoad); $i ++){
+						$currDay = $foodLoad[$i]['Day'];
+						$currMeal = $foodLoad[$i]['Meal_num'];
+						if($currDay !== $prevDay || $i == 0){
+						switch($foodLoad[$i]['Day']){
+							case 1:
+								print "<tr><th>Sunday</th><th></th><th></th></tr>";
+								break;
+							case 2:
+								print "<tr><th>Monday</th><th></th><th></th></tr>";
+								break;
+							case 3:
+								print "<tr><th>Tuesday</th><th></th><th></th></tr>";
+								break;
+							case 4:
+								print "<tr><th>Wednesday</th><th></th><th></th></tr>";
+								break;
+							case 5:
+								print "<tr><th>Thursday</th><th></th><th></th></tr>";
+								break;
+							case 6:
+								print "<tr><th>Friday</th><th></th><th></th></tr>";
+								break;
+							case 7:
+								print "<tr><th>Saturday</th><th></th><th></th></tr>";
+								break;
+						}
+					}
+					if($currMeal !== $prevMeal || $i == 0){
+						switch($foodLoad[$i]['Meal_num']){
+							case 1:
+								print "<tr><td></td><td>Breakfast:</td><td>".$foodLoad[$i]['M_Name']."</td></tr>";
+								break;
+							case 2:
+								print "<tr><td></td><td>Lunch:</td><td>".$foodLoad[$i]['M_Name']."</td></tr>";
+								break;
+							case 3:
+								print "<tr><td></td><td>Dinner:</td><td>".$foodLoad[$i]['M_Name']."</td></tr>";
+								break;
+						}
+					}else{
+						print "<tr><td></td><td></td><td>".$foodLoad[$i]['M_Name']."</td></tr>";
+					}
+
+						$prevDay = $currDay;
+						$prevMeal = $currMeal;
+					}
+
+					print "</table>";				
+				}
+
+			}
+			?>
 		</div>
 		<div id="meal_plans" style="width:66%">
 			<h2>Current Meal Plan <!-- This is where  we will print the name from the DB--></h3>
-				<form name="mealPlan" method="?" id="mealPlan" action="" onsubmit="return planAction('food.php')" style="margin: auto;">
+				<!-- <form name="mealPlan"  id="mealPlan" onsubmit="pageUpdate('GET')" style="margin: auto;"> -->
 					<!-- populate this select with all existing meal plans -->
-					<select style="display: block; margin: 0 auto; margin-top: 5px; margin-bottom: 5px;">
-						<option>Chicken Only</option>
-						<option>Turkey Only</option>
+					<select id="mealPlan" style="display: block; margin: 0 auto; margin-top: 5px; margin-bottom: 5px;">
+						<option value="Chicken Only">Chicken Only</option>
+						<option selected="selected" value="Basic Plan">Basic Plan</option>
 						<option>Russia (potato only)</option>
 						<option>O A T S</option>
 					</select>
-					<input type="submit" name="submit1" value="Select Plan" style="display: block; margin: 0 auto; margin-bottom: 5px">
-				</form>
-				<form name="newPlan" id ="newPlan" method="?" action="" onsubmit="return planAction('food_plan.php')">
-					<input type="submit" name="submit" value="Create New Plan" style="display: block; margin: 0 auto">
-				</form>
+					<input type="submit" name="submit1" value="Select Plan" onclick="pageUpdate('POST')" style="display: block; margin: 0 auto; margin-bottom: 5px">
+				<!-- </form> -->
+				<!-- <form name="newPlan" id ="newPlan" method="?" action="" onsubmit="pageUpdate('POST')"> -->
+					<input type="submit" name="submit" value="Create New Plan" onclick="pageUpdate('POST')" style="display: block; margin: 0 auto">
+				<!-- </form> -->
 		</div> 
 	</body>
 </html>
--- src/food_update.php
+++ src/food_update.php
@@ -1,23 +1,51 @@
 <?php
 	
-	session_start()''
+	session_start();
 
 	function getPlan($planName){
 		global $db;
 
-		$query = "SELECT * FROM IN_NUTPLAN WHERE NUT_PLAN_NAME = ? ORDER BY Day ASC, Meal_num DESC;";
+		$query = "SELECT * FROM IN_NUTPLAN WHERE NUT_PLAN_NAME = ? ORDER BY Day ASC, Meal_num ASC;";
 
 		$statement = $db->prepare($query);
+		$statement->bind_param('s', $planName);
+		$statement->execute();
+		$data = array();
+		$results = $statement->get_result();
 
-		if (! empty($params)) {
-		$statement->bind_param(str_repeat('s', count($params)), ...$params);
+		while ($row = $results->fetch_assoc()) {
+			array_push($data, $row);
 		}
+	
+		return $data;
+
+	}
+
+	function getGroceries($plan, $planName){
+		global $db;
+
+		$query = "SELECT Ingredient_Name, SUM(Ingredient_Amount) as Amount FROM Recipe WHERE Meal_Name in (Select M_Name From IN_NUTPLAN where Nut_Plan_Name = ?) GROUP BY Ingredient_Name;";
+
+		
+		$data = array();
+
+		$statement = $db->prepare($query);
+		$statement->bind_param('s', $planName);
 		$statement->execute();
 
 		$results = $statement->get_result();
 
-		return $results;
+		while ($row = $results->fetch_assoc()) {
+			array_push($data, $row);
+		}
 
+		$newdata = array();
+
+		for($i = 0; $i < count($data); $i++){
+			$newdata[$data[$i]['Ingredient_Name']] = $data[$i]['Amount'];
+		}
+
+		return $newdata;
 	}
 
 	function changeMealPlan($planName){
@@ -27,10 +55,10 @@
 
 		$statement = $db->prepare($query);
 
-		$statement->bind_param("ss", $planName, $_SESSION['username']);
+		$statement->bind_param('ss', $planName, $_SESSION['myusername']);
 		$statement->execute();
 
-		$results = $statement->execute();
+		$results = $statement->get_result();
 
 		return $results;
 	}
@@ -38,7 +66,9 @@
 	function createPlan($plan){
 		global $db;
 
-		$query
+		$query = "INSERT INTO `Nutrition Plan` (Nutrition_Plan_Name) VALUES ?;";
+
+		$meals = "INSERT INTO IN_NUTPLAN (Nut_Plan_Name, M_Name, Day, Meal_num) Values (?,?,?,?);";
 	}
 
 	function createMeal($meal){
@@ -46,6 +76,16 @@
 	}
 
 	function getMeal($mealName){
+		global $db;
+		$query = "SELECT * FROM Meal WHERE Meal_Name = ?;";
+
+		$statement = $db->prepare($query);
+
+		$statement->bind_param('s', $mealName);
+
+		$statement->execute();
+
+		$results = $statement->get_result();
 		
 	}
 
@@ -54,15 +94,33 @@
 	}
 
 	function getIngredient($ingredientName){
+		global $db;
+		$query = "SELECT * FROM Ingredient WHERE Ingredient_Name = ?;";
 
+		$statement = $db->prepare($query);
+
+		$statement->bind_param('s', $ingredientName);
+
+		$statement->execute();
+
+		$results = $statement->get_result();
 	}
 
 	function createRecipe($recipe){
 
 	}
 
-	function getRecipe($recipeName){
+	function getRecipe($recipe){
+		global $db;
+		$query = "SELECT * FROM Recipe WHERE Meal_Name = ? AND Ingredient_Name = ?;";
 
+		$statement = $db->prepare($query);
+
+		$statement->bind_param('s', $mealName);
+		#gonna need a for loop to get all the ingredients for a given recipe
+		$statement->execute();
+
+		$results = $statement->get_result();
 	}
 
 	$db = new mysqli('LOCALHOST', 'root', '', 'FitnessTracker');
@@ -73,6 +131,13 @@
 			if(isset($_REQUEST['planName']) && 0 < strlen($_REQUEST['planName'])){
 				$data = getPlan($_REQUEST['planName']);
 				changeMealPlan($_REQUEST['planName']);
+				if(!isset($data)){
+					http_response_code(404);
+					die();
+				}
+			}
+			else if (isset($_REQUEST['ingredientList']) && 0 < strlen($_REQUEST['ingredientList']) && isset($_REQUEST['mealName']) && 0 < strlen($_REQUEST['mealName'])) {
+				$data = getRecipe($_REQUEST['recipeName']);
 				if(!isset($data)){
 					http_response_code(404);
 					die();
@@ -92,35 +157,53 @@
 					die();
 				}
 			}
-			else if (isset($_REQUEST['recipeName']) && 0 < strlen($_REQUEST['recipeName'])) {
-				$data = getRecipe($_REQUEST['recipeName']);
-				if(!isset($data)){
-					http_response_code(404);
-					die();
-				}
+			if(!isset($data)){
+				http_response_code(404);
+				die();
 			}
 
 			echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 			break;
 
 		case 'POST':
+			$data = json_decode(file_get_contents('php://input'), true);
+
+				
 			if(isset($_REQUEST['plan']) && strlen($_REQUEST['plan']) > 0){
-				$plan = json_decode(file_get_contents('php://input'), true);
 				createPlan($plan);
+				http_response_code(201);
+			}
+			else if(isset($data['planName']) && 0 < strlen($data['planName'])){
+
+				changeMealPlan($data['planName']);
+
+				$info = getPlan($data['planName']);
+				$groceries = getGroceries($info, $data['planName']);
+
+				$data = array();
+
+				array_push($data, $info);
+				array_push($data, $groceries);
+
+				http_response_code(201);
+
+				echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+				
 			}
 			else if(isset($_REQUEST['meal']) && strlen($_REQUEST['meal']) > 0){
-				$meal = json_decode(file_get_contents('php://input'), true);
 				createMeal($meal);
+				http_response_code(201);
 			}
 			else if(isset($_REQUEST['ingredient']) && strlen($_REQUEST['ingredient']) > 0){
-				$ingredient = json_decode(file_get_contents('php://input'), true);
 				createIngredient($ingredient);
+				http_response_code(201);
 			}
 			else if(isset($_REQUEST['recipe']) && strlen($_REQUEST['recipe']) > 0){
 				$recipe = json_decode(file_get_contents('php://input'), true);
 				createRecipe($recipe);
+				http_response_code(201);
 			}
-			http_response_code(201);
+			// http_response_code(201);
 			break;
 
 	}
